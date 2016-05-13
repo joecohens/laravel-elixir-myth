574d8ae4848ca0a3fd564e48a9ab551dfcc83480
Joseph Cohen <joseph.cohen@dinkbit.com>
2016-05-13 18:00:17 -0500

    Use Elixir css extension and added tests

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
-var gulp = require('gulp'),
-    myth = require('gulp-myth'),
-    _ = require('underscore'),
-    Elixir = require('laravel-elixir');
+var gulp    = require('gulp');
+var Elixir  = require('laravel-elixir');
+var compile = require('laravel-elixir/tasks/shared/Css');
 
-var $ = Elixir.Plugins;
 var config = Elixir.config;
 
-Elixir.extend('myth', function (src, options) {
-    options = _.extend({
-        debug:     ! config.production,
-        srcDir:    config.get('assets.css.folder'),
-        outputDir: config.get('public.css.outputFolder'),
-    }, options);
+/*
+ |----------------------------------------------------------------
+ | Myth Compilation Task
+ |----------------------------------------------------------------
+ |
+ | This task will compile your Myth, including minification and
+ | and auto-prefixing. Additionally it supports any gulp myth
+ | options that you want to include with your compilation.
+ |
+ */
 
-    var paths = prepGulpPaths(src, options.srcDir, options.outputDir);
+Elixir.extend('myth', function(src, output, options) {
+    config.css.myth = {
+        folder: 'css'
+    };
 
-    new Elixir.Task('myth', function () {
-        this.log(paths.src, paths.output);
-        
-        return (
-            gulp.src(src)
-                .pipe(myth(options))
-                .on('error', function(e) {
-                    new Elixir.Notification('Webpack Compilation Failed!');
+    new Elixir.Task('myth', function() {
+        var paths = prepGulpPaths(src, output);
 
-                    this.emit('end');
-                })
-                .pipe($.if(! options.debug, $.uglify()))
-                .pipe(gulp.dest(paths.output.baseDir))
-                .pipe(new Elixir.Notification('Webpack Compiled!'))
-        );
+        return compile({
+            name: 'Myth',
+            compiler: require('gulp-myth'),
+            src: paths.src,
+            output: paths.output,
+            task: this,
+            pluginOptions: options || config.css.myth.pluginOptions
+        });
     })
-    .watch(config.get('assets.css.folder') + '/**/*.css');
+    .watch(config.get('assets.css.myth.folder') + '/**/*.css');
 });
+
 
 /**
  * Prep the Gulp src and output paths.
  *
- * @param {string|array} src
- * @param {string|null}  baseDir
- * @param {string|null}  output
+ * @param  {string|array} src
+ * @param  {string|null}  output
+ * @return {object}
  */
-var prepGulpPaths = function(src, baseDir, output) {
+var prepGulpPaths = function(src, output) {
     return new Elixir.GulpPaths()
-        .src(src, baseDir)
-        .output(output, 'app.css');
+        .src(src, config.get('assets.css.myth.folder'))
+        .output(output || config.get('public.css.outputFolder'), 'app.css');
 };
